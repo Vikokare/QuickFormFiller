be0344bc8d846518ef7023a97e2ed582712eecc0
Vaibhav Kokare <vaibhavkokare321@gmail.com>
2024-12-08 13:46:11 +0530

    Refactored base code

--- content.js
+++ content.js
@@ -1,10 +1,8 @@
-chrome.storage.sync.get(["name", "email"], (data) => {
-    document.querySelectorAll("input").forEach((input) => {
-        if (input.type === "text" && data.name) {
-            input.value = data.name;
-        }
-        if (input.type === "email" && data.email) {
-            input.value = data.email;
-        }
-    });
+document.querySelectorAll("input").forEach((input) => {
+    if (input.type === "text" && data.name) {
+        input.value = "ABC";
+    }
+    if (input.type === "email" && data.email) {
+        input.value = "ABC@gmail.com";
+    }
 });
