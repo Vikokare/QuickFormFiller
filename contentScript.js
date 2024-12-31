f45fe53d25c417bc9371ab37d2f9a0d865de428c
Vaibhav Kokare <vaibhavkokare321@gmail.com>
2024-12-31 12:36:40 +0530

    Form Fill using value att

--- contentScript.js
+++ contentScript.js
@@ -1,30 +1,44 @@
 console.log("Content script loaded");
 
 (() => {
-    document.querySelectorAll("input[type='submit']").forEach(e => 
-        createFlashBtn(e)
+    document.querySelectorAll("input[type='submit']").forEach(ele => 
+        createFlashBtn(ele)
     )
-    
-    function createFlashBtn(ele) {
-        childBtnHeight = ele.offsetHeight;
-
-        const flashBtn = document.createElement('img');
-        // flashBtn.src = chrome.runtime.getURL("assets/flashIcon");
-        flashBtn.src = "https://cdn-icons-png.flaticon.com/512/6645/6645280.png";
-        flashBtn.className = ele.className + "FlashBtn";
-        flashBtn.title = "Click to Quickform";
-        flashBtn.style.height = `${childBtnHeight}px`
-        flashBtn.style.width = 'auto'
-    
-        ele.parentElement.appendChild(flashBtn);
-        console.log("Element Added...");
-
-        flashBtn.addEventListener("click", () => {
-            checkFormInput(ele);
-        });
-    };
-
-    function checkFormInput(ele) {
-        console.log("---", ele.className, "---")
-    }
 })();
+
+
+function createFlashBtn(ele) {
+    submitBtnHeight = ele.offsetHeight;
+
+    const flashBtn = document.createElement('img');
+    flashBtn.src = chrome.runtime.getURL("assets/flashIcon.png");
+    // flashBtn.src = "https://cdn-icons-png.flaticon.com/512/6645/6645280.png";
+    flashBtn.className = ele.className + "FlashBtn";
+    flashBtn.title = "Supercharge Form";
+    flashBtn.style.height = `${submitBtnHeight}px`;
+    flashBtn.style.width = 'auto';
+
+    ele.parentElement.appendChild(flashBtn);
+    console.log("Element Added...");
+
+    flashBtn.addEventListener("click", () => {
+        processInputTags(ele.closest("form"));
+    });
+};
+
+function processInputTags(formEle) {
+    const dictData = {
+        "FirstName": "ABC",
+        "LastName": "XYZ",
+        "Email": "abc@gmail.com",
+        "Company": "IJK",
+        "Country": "1234567890",
+    }
+    const keys = Object.keys(dictData)
+
+    // console.log("---", formEle.className, "---");
+    formEle.querySelectorAll("input[type='text']").forEach(inp => {
+        const inputLabel = inp.placeholder || inp.name || inp.closest("div").textContent.trim()
+        inp.value = dictData[inputLabel]
+    })
+};
